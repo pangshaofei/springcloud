a20102220f595acfdda9c018da5a319118bf79c6
Polaris <polarispsf@gmail.com>
2018-07-19 01:34:25 +0800

    fix

--- test-server/src/test/java/com/polaris/testserver/TestServerApplicationTests.java
+++ test-server/src/test/java/com/polaris/testserver/TestServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.polaris.demo1;
+package com.polaris.testserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Demo1ApplicationTests {
+public class TestServerApplicationTests {
 
     @Test
     public void contextLoads() {
